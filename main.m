8e1b619d769c5e0dbc687deba1cbbac512b6d2e2
Romain Tajan <romain.tajan@ims-bordeaux.fr>
2018-10-05 08:27:47 +0200

    Update de main.m.

--- main.m
+++ main.m
@@ -44,7 +44,7 @@ message_destination = BinaryFileWriter('DataType','uint8');
 ber = zeros(1,length(channel_params.EbN0dB));
 Pe = qfunc(sqrt(2*channel_params.EbN0));
 tx_rx_flt_delay = frame_bit_sz - waveform_params.rxflt.FilterSpanInSymbols*waveform_params.mod.ModulationBPS;
-
+%% Preparation de l'affichage
 figure(1)
 semilogy(channel_params.EbN0dB,Pe);
 hold all
@@ -55,39 +55,52 @@ xlabel('$\frac{E_b}{N_0}$ en dB','Interpreter', 'latex', 'FontSize',14)
 ylabel('$P_b$, TEB','Interpreter', 'latex', 'FontSize',14)
 legend({'$P_b$ (Th\''eorique)', 'TEB (Exp\''erimentale)'}, 'Interpreter', 'latex', 'FontSize',14);
 
+msg_format = '|   %7.2f  |   %7d   |  %7d | %2.2e |  %8.2f kO/s |   %8.2f kO/s |\n';
+
+fprintf(      '|------------|-------------|----------|----------|----------------|-----------------|\n')
+msg_header =  '|  Eb/N0 dB  |   Bit nbr   |  Bit err |   TEB    |    Debit Tx    |     Debit Rx    |\n';
+fprintf(msg_header);
+fprintf(      '|------------|-------------|----------|----------|----------------|-----------------|\n')
+
+
+%% Simulation
 for i_snr = 1:length(channel_params.EbN0dB)
     if store_rx_vid
         message_destination.release;
         message_destination.Filename = [rx_vid_prefix, num2str(channel_params.EbN0dB(i_snr)),'dB.ts'];
     end
-    
+    reverseStr = ''; % Pour affichage en console
     awgn_channel.EbNo=channel_params.EbN0dB(i_snr);% Mise a jour du EbN0 pour le canal
     
     stat_erreur.reset; % reset du compteur d'erreur
     err_stat = [0 0 0];
     while (err_stat(2) < 100 && err_stat(3) < 1e6)
         message_source.reset;
+        T_rx = 0;
+        T_tx = 0;
         while(~message_source.isDone)
             %% Emetteur
+            tic
             tx_oct     = step(message_source); % Lire une trame
             tx_scr_oct = bitxor(tx_oct,dvb_scramble); % scrambler
             tx_scr_bit = step(o2b,tx_scr_oct); % Octets -> Bits
             tx_sym     = step(mod_psk,  tx_scr_bit); % Modulation QPSK
-            tx_sps    = step(tx_filter,tx_sym);
-            
+            tx_sps     = step(tx_filter,tx_sym);
+            T_tx       = T_tx+toc;
             %% Canal
             tx_sps_dpl = step(doppler, tx_sps); % Simulation d'un effet Doppler
             rx_sps_del = step(channel_delay, tx_sps_dpl, channel_params.Delai); % Ajout d'un retard de propagation
             rx_sps     = step(awgn_channel,channel_params.Gain * rx_sps_del); % Ajout d'un bruit gaussien
             
             %% Recepteur
+            tic
             rx_sym      = step(rx_filter,rx_sps);
             rx_scr_llr  = step(demod_psk,rx_sym);% Ce bloc nous renvoie des LLR (meilleur si on va interface avec du codage)
             rx_scr_bit  = rx_scr_llr<0; % Bits
             rx_scr_sync = step(mac_sync_delay, rx_scr_bit, tx_rx_flt_delay); % synchronisation couche acces.
             rx_scr_oct  = step(b2o,rx_scr_sync); % Conversion en octet pour le scrambler
             rx_oct      = bitxor(rx_scr_oct,dvb_scramble); % descrambler
-            
+            T_rx        = T_rx + toc;
             %% Compate des erreurs binaires
             tx_bit     = step(o2b,tx_oct);
             rx_bit     = step(o2b,rx_oct);
@@ -98,6 +111,10 @@ for i_snr = 1:length(channel_params.EbN0dB)
                 step(message_destination, rx_oct); % Ecriture du fichier
             end
         end
+		msg = sprintf(msg_format,channel_params.EbN0dB(i_snr), err_stat(3), err_stat(2), err_stat(1), err_stat(3)/8/T_tx/1e3, err_stat(3)/8/T_rx/1e3);
+		fprintf(reverseStr);
+		msg_sz =  fprintf(msg);
+		reverseStr = repmat(sprintf('\b'), 1, msg_sz);        
     end
     if err_stat(2) == 0
         break
@@ -106,6 +123,7 @@ for i_snr = 1:length(channel_params.EbN0dB)
     refreshdata(h_ber);
     drawnow limitrate
 end
+fprintf(      '|------------|-------------|----------|----------|----------------|-----------------|\n')
 
 
 
